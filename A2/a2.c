17563c535d027c4c5d68fb35f37599a7d2b38ea5
cruemartin92 <crue.c.martin@wmich.edu>
2019-03-18 22:56:04 -0400

    Finnished A2

--- A2/a2.c
+++ A2/a2.c
@@ -1,110 +1,199 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "a2.h"
-
+//#include "a2.h"
+#include "printStats.c"
 
 #define COUNT 48
 
-
-void createStructs(process *proc);
-// void setupReadyQueue(unsigned int * q);
-void enqueue(unsigned int *q, unsigned int *qCount, int process);
-unsigned int dequeue(unsigned int *q, unsigned int *qCount);
-
+void readFile(process *proc);
+void bubbleSort(process *proc, ui *queue, ui queueCount);
+void cpuCycle(process *proc, ui *io, ui ioCount, ui cpu, os OS);
+void loop(process *proc, ui * queue, ui * io, ui cpu, ui queueCount, ui ioCount, os OS);
 
 int main(int argc, char *agrv[]){
 
-	unsigned int *q = malloc(COUNT);
-	unsigned int *io = malloc(COUNT);
-	unsigned int qCount =0;
-	unsigned int ioCount =0;
 
-	//Allocate memory for the pointer to structs
-    process *proc = malloc(sizeof(process)*COUNT);
+   
+    //Varibales and arrays
+    process proc[48]; 
+    ui queue[48]; 
+    ui queueCount = 0;
+    ui io[48]; 
+    ui ioCount = 0;
+    ui cpu;
 
-	//Called createStructs to create and fill stucts 
-	createStructs(proc);
+    os OS;
+
+    OS.quantum = 70;
+    OS.wait = 30;
+
     
-	//set up prority queue
+	//read in the file
+	readFile(proc);
+
+	// do the queue stuff
+
+	for(int i=0; i < COUNT; i++){
+		queue[i]=i;
+	}
+	queueCount = COUNT;
+
+	// sort the queue
+	bubbleSort(proc, queue, queueCount);
+	//do the loop
+	
+	loop(proc, queue, io, cpu, queueCount, ioCount, OS);
+
+	//print the stats
+
+	printStats(proc, OS);
+    
+    
 
     return 0;
 }
 
-void createStructs(process *proc){
+void readFile(process *proc){
 	int i =0;
+	 //File stuff
+    char *testFile = "testinput";
 
-	//Allocate memory for the varibles of each struct
-    for(i; i<COUNT; i++){
-         proc[i].priority = malloc(sizeof(ui)); // never changes
-	     proc[i].cpu = malloc(sizeof(ui));  // time in cpu before I/O
-	     proc[i].io = malloc(sizeof(ui)); // time I/O takes
-	     proc[i].runTime = malloc(sizeof(ui)); // how long process runs
+    FILE *file;
 
-	     proc[i].curCpu = malloc(sizeof(ui)); // count of current time in CPU
-	     proc[i].curIo = malloc(sizeof(ui));  // count of time waiting for I/O
-	     proc[i].wait = malloc(sizeof(ui)); // current count of time in ready queue
-	     proc[i].curPrior = malloc(sizeof(ui)); // adjusted for starvation
-	     proc[i].cpuTotal = malloc(sizeof(ui)); // sum of time in cpu
-	     proc[i].ioTotal = malloc(sizeof(ui)); // sum of time doing io
-				// statistics
-	     proc[i].waitSum = malloc(sizeof(ui)); // total time in ready queue
-	     proc[i].waitCount = malloc(sizeof(ui)); // how many times in ready queue (for average)
-	     proc[i].waitMin = malloc(sizeof(ui)); // smallest time in ready queue
-	     proc[i].waitMax = malloc(sizeof(ui)); // longet time in ready queu
-    }
+    file = fopen(testFile, "r");
 
-	i =0;
-
-	// open file
-
-    FILE * file;
-          
-    file = fopen("testinput", "r");
-
-	//read in the information from the file and intialize the stuct variables 
-    while(fscanf(file, "%u %u %u %u", &proc[i].priority, &proc[i].cpu, &proc[i].io, &proc[i].runTime) != -1){
-
-        //fprintf(stdout,"%d\n", i);
-
-        proc[i].curCpu =0; // count of current time in CPU
-	    proc[i].curIo =0;  // count of time waiting for I/O
-	    proc[i].wait =0; // current count of time in ready queue
-	    proc[i].curPrior =0; // adjusted for starvation
-	    proc[i].cpuTotal =0; // sum of time in cpu
-	    proc[i].ioTotal =0; // sum of time doing io
-				// statistics
-	    proc[i].waitSum =0; // total time in ready queue
-	    proc[i].waitCount =0; // how many times in ready queue (for average)
-	    proc[i].waitMin =0; // smallest time in ready queue
-	    proc[i].waitMax =0; // longet time in ready queu
-
-        i++;
-
-    }
-
-    fclose(file);
-}
-
-// void setupReadyQueue(unsigned int * q){
-// 	for(int i=0; i<COUNT; i++){
-// 		q[i]=i;
-// 	}
-// }
-
-void enqueue(unsigned int *q, unsigned int *qCount, int process){
-	q[(unsigned int)*qCount] = process;
-}
-
-unsigned int dequeue(unsigned int *q, unsigned int *qCount){
-	unsigned int oldFront = q[0];
-
-	for(int i=0; i<*qCount; i++){
-
-		q[i] = q[i+1];
+	if(file == NULL){
+		printf("ERROR OPENING FILE\n");
+		exit(1);
 	}
 
-	*qCount = *qCount-1;
+	for(i; i< COUNT; i++){
+		fscanf(file, "%u %u %u %u", &proc[i].priority, &proc[i].cpu, &proc[i].io, &proc[i].runTime);
+		proc[i].waitCount =1;
+        proc[i].curPrior = proc[i].priority;
+	}
 
-	return oldFront;
+	// while(fscanf(file, "%u %u %u %u", &proc[i].priority, &proc[i].cpu, &proc[i].io, &proc[i].runTime) != -1){
+    //     proc[i].waitCount =1;
+    //     proc[i].curPrior = proc[i].priority;
+    // }
+
+}
+
+void bubbleSort(process *proc, ui *queue, ui queueCount){
+	ui temp;
+
+	for(int i=0; i< queueCount; i++ ){
+		if(proc[i].curPrior > proc[i+1].curPrior){
+			temp = queue[i+1];
+			queue[i+1]= queue[i];
+			queue[i] = temp;
+		}
+	}
+}
+
+void cpuCycle(process *proc, ui *io, ui ioCount, ui cpu, os OS){
+	int i =0;
+
+	ui index;
+
+	//io
+	while(i < ioCount){
+		index = io[i];
+		proc[index].curIo++;
+		proc[index].waitSum++;
+	}
+
+	//queue
+	for(i =0; i > COUNT; i++){
+		proc[i].wait++;
+		proc[i].waitSum++;
+
+		if(proc[i].wait >= OS.wait){
+			proc[i].curPrior++;
+			proc[i].wait =0;
+		}
+	}
+
+	//cpu 
+	proc[cpu].curCpu++;
+	proc[cpu].cpuTotal++;
+}
+
+void loop(process *proc, ui * queue, ui * io, ui cpu, ui queueCount, ui ioCount, os OS){
+
+	while((cpu != COUNT) && (ioCount >0) && (queueCount > 0)){
+
+		ui hPriority = queue[queueCount]; // get highes priority in queue
+
+		// load cpu is empty load next ready process
+
+		if((cpu == COUNT) && (queueCount >0)){
+			cpu = hPriority;
+			queueCount--;
+
+			if(proc[cpu].waitMax < proc[cpu].wait){
+				proc[cpu].waitMax = proc[cpu].wait;
+			}
+			else if(proc[cpu].waitMin > proc[cpu].wait){
+				proc[cpu].waitMin = proc[cpu].wait;
+			}
+		}
+
+		//move process from cpu
+
+		if((proc[cpu].curCpu == proc[cpu].cpu) || (proc[cpu].curCpu == OS.quantum)){
+
+			if(proc[cpu].cpu < OS.quantum){
+				io[ioCount] = cpu;
+				ioCount++;
+			}
+			else{ // move out of the cpu
+				proc[cpu].wait =0;
+				proc[cpu].waitCount++;
+				queue[queueCount] = cpu;
+				queueCount++;
+
+				bubbleSort(proc, queue, queueCount);
+			}
+
+			// set cpu 
+			proc[cpu].curCpu =0;
+			cpu = COUNT; 
+		}
+
+		//update IO
+
+		for(int i =0; i < COUNT; i++){
+			ui x = io[i];
+
+			if((x != COUNT) && (proc[x].curIo == proc[x].io)){
+				//check runtime
+				if(proc[x].runTime <= (proc[x].ioTotal + proc[x].cpuTotal)){
+					ioCount--;
+				}
+				else{
+					//put io in the queue
+					proc[x].waitCount;
+					proc[x].wait =0;
+					queueCount++;
+					bubbleSort(proc, queue, queueCount);
+					ioCount--;
+				}
+				io[i] = COUNT;
+			}
+		}
+
+		// do a cycle
+		cpuCycle(proc, io, ioCount, cpu, OS);
+
+		//sort the queue
+		bubbleSort( proc, queue, queueCount);
+
+
+	}
+
+
 }
